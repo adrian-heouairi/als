4d9960cf0b2cf1172fea83beeaa28273bb3f81aa
Adrian Heouairi <adrian.heouairi@gmail.com>
2024-05-11 21:47:36 +0200

    Update

--- repository/test/upgrade.sh
+++ repository/test/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source "$(dirname "$(dirname "$(realpath -- "$BASH_SOURCE")")")"/source/als-packages.sh || exit 1
+source "$(dirname "$(dirname "$(dirname "$(realpath -- "$BASH_SOURCE")")")")"/source/als-packages.sh || exit 1
 
 echo test package upgraded "$ALS_FULLPATH"
