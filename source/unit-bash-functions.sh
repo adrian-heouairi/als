7ddba871a017ecef578cfc7f1a8d006b361e3c54
Adrian Heouairi <adrian.heouairi@gmail.com>
2024-05-11 19:57:52 +0200

    Update

--- source/unit-bash-functions.sh
+++ source/unit-bash-functions.sh
@@ -7,8 +7,8 @@ print-debug point x1
     unset "$to_var_name"
     declare -g -a "$to_var_name"
 print-debug point x2
-    local i c=0
-    for i in "${!from_var_name[@]}"; do
+    local i c=0 from_var_name_at=$from_var_name"[@]"
+    for i in "${!from_var_name_at}"; do
         print-debug point x3 "i = $i, from_var_name = $from_var_name, to_var_name = $to_var_name, c = $c"
         printf -v "${to_var_name}[$((c++))]" %s "$i"
         print-debug point x4 "c = $c"
