e2c9d9ad5363b487c5df49fded51abd0e435272e
Adrian Heouairi <adrian.heouairi@gmail.com>
2024-05-11 19:47:19 +0200

    Update

--- source/unit-bash-functions.sh
+++ source/unit-bash-functions.sh
@@ -3,12 +3,13 @@
 copy_array() {
     local from_var_name=$1
     local to_var_name=$2
-
+print-debug point x1
     unset "$to_var_name"
     declare -g -a "$to_var_name"
-
+print-debug point x2
     local i c=0
     for i in "${!from_var_name[@]}"; do
+        print-debug point x3
         printf -v "${to_var_name}[$c]" %s "$i"
         ((c++))
     done
